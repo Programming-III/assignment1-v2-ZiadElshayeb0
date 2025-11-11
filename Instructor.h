cf2d63efbe82fd56f3258a6a47737020f0acd7c4
Ziad Elshayeb <ziad.elshayeb@student.giu-uni.de>
2025-11-11 09:57:34 +0200

    Final

--- Instructor.h
+++ Instructor.h
@@ -2,18 +2,24 @@
 #define INSTRUCTOR_H
 
 #include "Person.h"
+#include <string>
+
 using namespace std;
+
 //#write Instructor class here
 
+class Instructor : public Person{
 
+    protected:
+    string department;
+    int experienceYears;
 
+    public:
+    Instructor(string name, int id, string department, int experienceYears);
+    Instructor();
+    ~Instructor();
+    void display();
 
-
-
-
-
-
-
-
+};
 
 #endif

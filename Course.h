91ba998424f9a2d1f1b93250ffbafe4d869c48c4
Ziad Elshayeb <ziad.elshayeb@student.giu-uni.de>
2025-11-11 09:57:45 +0200

    Final

--- Course.h
+++ Course.h
@@ -3,20 +3,26 @@
 
 #include <string>
 #include "Student.h"
+
 using namespace std;
+
 //#write your code here
 
+class Course {
 
+    private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
 
-
-
-
-
-
-
-
-
-
-
+    public:
+    Course(string courseCode, string courseName, int maxStudents, Student* students, int currentStudents);
+    Course();
+    ~Course();
+    int addStudent(const Student& s);
+    void displayCourseInfo();
+};
 
 #endif

0cb902020639eaa66d376fdc658e2eff23b06eb7
Ziad Elshayeb <ziad.elshayeb@student.giu-uni.de>
2025-11-11 09:57:23 +0200

    Final

--- Student.h
+++ Student.h
@@ -3,19 +3,23 @@
 
 #include "Person.h"
 #include <string>
-#using namespace std;
+
+using namespace std;
+
 //#write student class here
 
+class Student : public Person {
 
+    protected:
+    int yearLevel;
+    string major;
 
+    public:
+    Student(string name, int id, int yearLevel, string major);
+    Student();
+    ~Student();
+    void display();
 
-
-
-
-
-
-
-
-
+};
 
 #endif
